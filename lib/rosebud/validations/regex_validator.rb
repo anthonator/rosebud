94297b9caaca23f1bd0ffebab78877d2c9d391f3
Anthony Smith <anthony@sticksnleaves.com>
2013-11-21 23:14:00 -0500

    Fixed regex validation.

--- lib/rosebud/validations/regex_validator.rb
+++ lib/rosebud/validations/regex_validator.rb
@@ -2,7 +2,7 @@ module Rosebud
   module Validations
     class RegexValidator < Validator
       def validate_param(name, value, pattern)
-        error!(:regex, param: name) unless value.to_s =~ pattern
+        error!(:regex, param: name) if value && (value.to_s =~ pattern).nil?
       end
     end
     
